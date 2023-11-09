c26675f22c613e3c233b8db91f34e582dbd3162b
Abay <ibrayev.abay@gmail.com>
2023-11-08 19:44:08 -0700

    react router dom

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import LoginPage from '../Login/LoginPage';
 
 export default function App() {
+  const [user, setUser] = useState(null)
   return (
     <main className="App">
       React Movies
