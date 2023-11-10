45ea1b364a2c85a24a7818808bd3eca9317ddcc5
Abay <ibrayev.abay@gmail.com>
2023-11-09 19:24:05 -0700

    actor cards

---
+++ src/components/ActorCard/ActorCard.jsx
@@ -0,0 +1,5 @@
+const image = 'https://cdn.vox-cdn.com/thumbor/KwXyAUZGjHOfbZ7Ef0AAtXnzG7M=/0x0:1409x785/1400x1050/filters:focal(734x364:735x365)/cdn0.vox-cdn.com/uploads/chorus_asset/file/8846551/Screen_Shot_2017_07_13_at_1.09.20_PM.png'
+
+export default function ActorCard({ cast }) {
+
+}
--- src/pages/ActorList/ActorListPage.jsx
+++ src/pages/ActorList/ActorListPage.jsx
@@ -1,5 +1,24 @@
+import { movies } from "../../data";
+
 export default function ActorListPage() {
+    let casts = []
+    movies.forEach((m, idx) => (
+        casts = casts.concat(m.cast)
+    ))
+
+    const castSet = new Set(casts);
+    const castArray = Array.from(castSet)
+    console.log(casts)
+    const castCards = castArray.map((a, idx) => (
+        <li>
+            <h4>{a}</h4>
+        </li>
+    ))
+    
     return (
-        <h1>Actor list</h1>
+        <main>
+            <h1>Actor list</h1>
+            <ul>{castCards}</ul>
+        </main>
     )
 }
